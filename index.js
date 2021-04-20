f7db4bd0eca22f8d27579095af6143a474cb7bba
EitanFuturo <llovia@gmail.com>
2021-04-19 22:07:17 -0300

    Se agrega un return para que no se creen varios clocks en paralelo y se modifica el texto del botón según el estado del clock.

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
+let clock, startButton
+
 window.onload = () => {
-  const clock = document.getElementById('clock')
+  initializeConstants()
 }
+
+let newClockTriggered = true;
 
 const countdown = (bpm=90, numberOfPatterns=6) => {
     let numberOfBeats = numberOfPatterns * 16
+
     let seconds = (numberOfBeats * 60) / bpm
    
     showCountdown(seconds)
 }
 
 const showCountdown = (seconds) => {
+    if (newClockTriggered) {
+      return
+    }
+
     clock.innerHTML = seconds
     seconds --
 
     if (seconds >= 0) {
-        setTimeout( () => showCountdown(seconds), 1000)
+      setTimeout( () => showCountdown(seconds), 1000)
     }
 }
 
 const playCountdown = () => {
   let bpm = parseInt(document.getElementById('input-bpm').value)
   let patterns = parseInt(document.getElementById('input-patterns').value)
+  
+  newClockTriggered = !newClockTriggered
 
+      switch(startButton.innerHTML) {
+        case 'Iniciar':  
+          startButton.innerHTML = 'Parar'
+          break
+        case 'Reiniciar':  
+          startButton.innerHTML = 'Parar'
+          break
+        case 'Parar':
+          startButton.innerHTML = 'Reiniciar'
+      }
   countdown(bpm, patterns)
+}
+
+const initializeConstants = () => {
+  clock = document.getElementById('clock')
+  startButton = document.getElementById('start-button')
+  console.log(clock, startButton)
 }
